f43fbad78554e2ad2704d8ef770e8b23e1f21c02
Jasmina Vasiljevic <jasmina.vasiljevic@gmail.com>
2025-12-14 20:39:55 -0800

    update

--- design_patterns/tools/tool_1/code/pkg_agent_chat/package/setup.py
+++ design_patterns/tools/tool_1/code/pkg_agent_chat/package/setup.py
@@ -4,7 +4,7 @@ Setup configuration for pkg_agent_chat.
 
 from setuptools import setup, find_packages
 
-with open("code/README.md", "r", encoding="utf-8") as fh:
+with open("../README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
